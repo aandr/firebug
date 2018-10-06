1a013ddf11cd0209f47cff0037473a5730594799
Alek Andreev <alek@zvuk.net>
2018-10-06 00:29:41 -0700

    frontend updates

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,23 @@
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import Chart from 'chart.js';
+import Leaflet from 'leaflet';
 import Vue from 'vue';
 import VueChartkick from 'vue-chartkick';
 import App from './App.vue';
 import Navbar from './components/Navbar.vue';
 import router from './router';
 import store from './store';
+
+
+delete Leaflet.Icon.Default.prototype._getIconUrl;
+
+
+Leaflet.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 Vue.config.productionTip = false
 
--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <l-map ref="map" :zoom=13 :center="[37.3863, -122.0669]">
+    <l-map ref="map" :zoom=13 :center="[37.3863, -122.0669]" style="width: 100%; height: 80%">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <l-marker 
         v-for="device in devices"
         :key="device.id"
-        :lat-lng="[device.lat, device.lng]" />
+        :lat-lng="[device.lat, device.lon]" />
     </l-map>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
 export default {
+  components: { LMap, LTileLayer, LMarker },
   data() {
     return {
       url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
