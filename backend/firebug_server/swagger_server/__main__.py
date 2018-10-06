f3d2b53ef17789139ccc35266cc8dc95cdd29534
Alek Andreev <alek@zvuk.net>
2018-10-06 11:46:04 -0700

    add CORS to backend

--- backend/firebug_server/swagger_server/__main__.py
+++ backend/firebug_server/swagger_server/__main__.py
@@ -2,12 +2,14 @@
 
 import os
 import connexion
+from flask_cors import CORS
 
 from swagger_server import encoder
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'FireBug API'})
     app.run(port=os.environ.get('PORT', 8080))
